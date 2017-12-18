2cbb1558db36553d66941ef3cc5e5940a0ece6dc
rojanu <aliustek@gmail.com>
2017-12-18 10:30:31 +0000

    fix sed command

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -6,7 +6,7 @@ if [[ ${TRAVIS_PULL_REQUEST} == false ]] ; then
         sbt ++${TRAVIS_SCALA_VERSION} release skip-tests
     else
         if [[ ${TRAVIS_BRANCH} != "develop" ]]; then
-            sed -i "s/SNAPSHOT/SNAPSHOT.${TRAVIS_BUILD_NUMBER}" ../version.sbt
+            sed -i "s/SNAPSHOT/SNAPSHOT.${TRAVIS_BUILD_NUMBER}/g" ../version.sbt
         fi
         sbt ++${TRAVIS_SCALA_VERSION} distribution/publish
     fi
