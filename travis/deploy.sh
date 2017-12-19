3b784b6f4ac8a32ca370cabac694285f3c6dd315
rojanu <aliustek@gmail.com>
2017-12-19 16:50:48 +0000

    debug

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -5,6 +5,8 @@ setup_git() {
     git config --global user.email "${GIT_USER_EMAIL}"
     git config --global user.name "${GIT_USER_NAME}"
     eval "$(ssh-agent -s)" # Start the ssh agent
+    ls -la ${TRAVIS_BUILD_DIR}
+    ls -la ${TRAVIS_BUILD_DIR}/travis
     chmod 600 ${TRAVIS_BUILD_DIR}/travis/rojanu_id_rsa
     ssh-add ${TRAVIS_BUILD_DIR}/travis/rojanu_id_rsa
 }
