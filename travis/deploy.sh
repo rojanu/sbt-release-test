0793a5f421c3bb869e81dc375a995c4236449202
rojanu <aliustek@gmail.com>
2017-12-19 14:32:27 +0000

    add with-defaults to release

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-set -ev
+set -e
 
 if [[ ${TRAVIS_PULL_REQUEST} == false ]] ; then
     if [[ ${TRAVIS_BRANCH} == master ]] ; then
         git checkout master
-        sbt ++${TRAVIS_SCALA_VERSION} release with-defaults skip-tests
+        sbt ++${TRAVIS_SCALA_VERSION} 'release with-defaults skip-tests'
     else
         if [[ ${TRAVIS_BRANCH} != "develop" ]]; then
             sed -i "s/-SNAPSHOT/.${TRAVIS_BUILD_NUMBER}-SNAPSHOT/g" ${TRAVIS_BUILD_DIR}/version.sbt
