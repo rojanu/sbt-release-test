6822e2d8b9c8520e7a88bfa2aacbc60f23bc8a6b
rojanu <aliustek@gmail.com>
2017-12-19 19:03:51 +0000

    increase version to 2018.1.2-SNAPSHOT

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -6,8 +6,6 @@ setup_git() {
     git config --global user.name "${GIT_USER_NAME}"
     eval "$(ssh-agent -s)" # Start the ssh agent
     openssl aes-256-cbc -K $encrypted_e91e22d15568_key -iv $encrypted_e91e22d15568_iv -in travis/rojanu_id_rsa.enc -out travis/rojanu_id_rsa -d
-    ls -la ${TRAVIS_BUILD_DIR}
-    ls -la ${TRAVIS_BUILD_DIR}/travis
     chmod 600 ${TRAVIS_BUILD_DIR}/travis/rojanu_id_rsa
     ssh-add ${TRAVIS_BUILD_DIR}/travis/rojanu_id_rsa
 }
@@ -16,7 +14,7 @@ if [[ ${TRAVIS_PULL_REQUEST} == false ]] ; then
     if [[ ${TRAVIS_BRANCH} == master ]] ; then
         git checkout master
         setup_git
-        sbt clean ++${TRAVIS_SCALA_VERSION} 'release with-defaults skip-tests'
+        sbt ++${TRAVIS_SCALA_VERSION} 'release with-defaults skip-tests'
     else
         if [[ ${TRAVIS_BRANCH} != "develop" ]]; then
             sed -i "s/-SNAPSHOT/.${TRAVIS_BUILD_NUMBER}-SNAPSHOT/g" ${TRAVIS_BUILD_DIR}/version.sbt
--- version.sbt
+++ version.sbt
@@ -1,1 +1,1 @@
-version in ThisBuild := "2018.1.1-SNAPSHOT"
+version in ThisBuild := "2018.1.2-SNAPSHOT"
