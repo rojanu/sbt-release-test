97158c6f2f9d7b61f86d5d682b1a17671069092c
rojanu <aliustek@gmail.com>
2017-12-19 15:17:06 +0000

    setup git

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
-set -e
+set -ev
+
+setup_git() {
+    git config --global user.email "${GIT_USER_EMAIL}"
+    git config --global user.name "${GIT_USER_NAME}"
+    eval "$(ssh-agent -s)" # Start the ssh agent
+    echo "${GIT_USER_KEY}" > ~/.ssh/deploy_key.pem
+    chmod 600 ~/.ssh/deploy_key.pem # This key should have push access
+    ssh-add ~/.ssh/deploy_key.pem
+}
 
 if [[ ${TRAVIS_PULL_REQUEST} == false ]] ; then
     if [[ ${TRAVIS_BRANCH} == master ]] ; then
         git checkout master
+        setup_git
         sbt ++${TRAVIS_SCALA_VERSION} 'release with-defaults skip-tests'
     else
         if [[ ${TRAVIS_BRANCH} != "develop" ]]; then
