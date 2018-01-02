4eee9f8401ecfabc260da037fe3b7404f6e94630
rojanu <aliustek@gmail.com>
2018-01-02 15:05:55 +0000

    test on branch

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -15,6 +15,6 @@ if [[ ${TRAVIS_PULL_REQUEST} == false ]] ; then
         if [[ ${TRAVIS_BRANCH} != develop ]]; then
             sed -i "s/-SNAPSHOT/.${TRAVIS_BUILD_NUMBER}-SNAPSHOT/g" ${TRAVIS_BUILD_DIR}/version.sbt
         fi
-        sbt ++${TRAVIS_SCALA_VERSION} distribution/publish
+        sbt ++${TRAVIS_SCALA_VERSION} publish
     fi
 fi
